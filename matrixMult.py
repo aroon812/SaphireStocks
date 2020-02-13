6c8459124da81688d0b3b93f065cdad6e4ee457d
Jewell Day <day348@gmail.com>
2020-02-13 15:45:26 -0800

    Changes to be committed: 	modified:   matrixMult.py
    Changes not staged for commit:
    	modified:   gradientDescent.py
    
    Untracked files:
    	__pycache__/
    	simple_test.py

--- matrixMult.py
+++ matrixMult.py
@@ -1,147 +1,146 @@
 import math;
 import numpy;
-class NeuralNetwork:
-    def calculateOutput(self, activationFunction, matrix, input, rando):
-        b=len(matrix);
-        listToBeReturned=[]
-        other=[]
-        for a in range(0,b):
-            listToBeReturned.append(input);
-            try:
-                output= matrixMultiplication(matrix[a], input)
-                other.append(output)
-            except ValueError: 
-                print("Matrices were invalid dimensions or...")
-            active= activationFunction[a]
-            if(active==1):
-                input=activationArcTan(output)
-            elif(active==2):
-                input=activationELU(output)
-            elif(active==3):
-                input=activationIdentity(output)
-            elif(active==4):
-                input=activationLeakyReLU(output)
-            elif(active==5):
-                input=activationRandomReLU(output, rando)
-            elif(active==6):
-                input=activationReLU(output)
-            elif(active==7):
-                input=activationSigmoid(output)
-            elif(active==8):
-                input=activationSoftPlus(output)
-            elif(active==9):
-                input=activationStep(output)
-            else:
-                input=activationTanH(output)
-        listToBeReturned.append(input)
-        return [listToBeReturned, other] 
-
-
-
-    def matrixMultiplication(self, weights, input):
-        outputSize= len(weights[0])
-        other= len(weights)
-        lenOfInput=len(input)
-        output=[]
-        bbool=True
-        for alpha in range(0, other):
-            if(len(weights[alpha])!=outputSize):
-                print(weights[alpha])
-                bbool=False
-        if(lenOfInput==other and bbool):
-            for a in range(0,outputSize):
-                sum=0
-                for b in range(0, other):
-                    sum+=input[b]*weights[b][a]
-                output.append(sum)
-            return output
+def calculateOutput( activationFunction, matrix, input, rando):
+    b=len(matrix);
+    listToBeReturned=[]
+    other=[]
+    for a in range(0,b):
+        listToBeReturned.append(input);
+        try:
+            output= matrixMultiplication(matrix[a], input)
+            other.append(output)
+        except ValueError: 
+            print("Matrices were invalid dimensions or...")
+        active= activationFunction[a]
+        if(active==1):
+            input=activationArcTan(output)
+        elif(active==2):
+            input=activationELU(output,rando)
+        elif(active==3):
+            input=activationIdentity(output)
+        elif(active==4):
+            input=activationLeakyReLU(output)
+        elif(active==5):
+            input=activationRandomReLU(output, rando)
+        elif(active==6):
+            input=activationReLU(output)
+        elif(active==7):
+            input=activationSigmoid(output)
+        elif(active==8):
+            input=activationSoftPlus(output)
+        elif(active==9):
+            input=activationStep(output)
         else:
-            if(bbool):
-                raise ValueError("Dimensions are incompatible")
-            else:
-                raise ValueError("Weights are not all same length")
-    def activationSigmoid(self,input):
-        for a in range(0,len(input)):
-            z=input[a]
-            z=(-1)*z
-            input[a]= 1/(1+math.exp(z))
-        return input
-    def activationTanH(self,input):
-        for a in range(0,len(input)):
-            z=input[a]
-            input[a]=math.tanh(z)
-        return input
-    def activationReLU(self,input):
-        for a in range(0,len(input)):
-            z=input[a]
-            if(z<0):
-                input[a]=0
-        return input
-    def derivReLU(self, input):
-        for a in range(0,len(input)):
-            z=input[a]
-            if(z<0):
-                input[a]=0
-            else:
-                input[a]=1
-        return input
-    def activationLeakyReLU(self, input):
-        b=0.01
-        for a in range(0,len(input)):
-            z=input[a]
-            if(z<0):
-                input[a]=z*b
-        return input
-    def derivLeakyReLU(self, input):
-        for a in range(0, len(input)):
-            z=input[a]
-            if(z<0):
-                input[a]=0.01
-            else:
-                input[a]=1
-        return input
-    def activationRandomReLU(self, input,b):
-        for a in range(0,len(input)):
-            z=input[a]
-            if(z<0):
-                input[a]=z*b
-        return input
-    def derivRandomReLU(self, input, b):
-        for a in range(0,len(input)):
-            z=input[a]
-            if(z<0):
-                input[a]=b
-            else:
-                input[a]=1
-        return input
-    def activationStep(self, input):
-        for a in range(0,len(input)):
-            z=input[a]
-            if(z<0):
-                input[a]=0
-            else:
-                input[a]=1
-        return input
-    def activationArcTan(self, input):
-        for a in range(0,len(input)):
-            input[a]=numpy.arctan(input[a])
-        return input
-    def activationSoftPlus(self, input):
-        for a in range(0,len(input)):
-            z=input[a]
-            b=math.log(1+math.exp(z))
-            input[a]=b
-        return input
-    def activationELU(self, input, b):
-        for a in range(0,len(input)):
-            z=input[a]
-            if(z<0):
-                q=b*(math.exp(z)-1)
-                input[a]=q
-        return input
-    def activationIdentity(self, input):
-        return input
-    def derivIdentity(self, input):
-        for a in range(0,len(input)):
+            input=activationTanH(output)
+    listToBeReturned.append(input)
+    return [listToBeReturned, other] 
+
+
+
+def matrixMultiplication( weights, input):
+    outputSize= len(weights[0])
+    other= len(weights)
+    lenOfInput=len(input)
+    output=[]
+    bbool=True
+    for alpha in range(0, other):
+        if(len(weights[alpha])!=outputSize):
+            print(weights[alpha])
+            bbool=False
+    if(lenOfInput==other and bbool):
+        for a in range(0,outputSize):
+            sum=0
+            for b in range(0, other):
+                sum+=input[b]*weights[b][a]
+            output.append(sum)
+        return output
+    else:
+        if(bbool):
+            raise ValueError("Dimensions are incompatible")
+        else:
+            raise ValueError("Weights are not all same length")
+def activationSigmoid(input):
+    for a in range(0,len(input)):
+        z=input[a]
+        z=(-1)*z
+        input[a]= 1/(1+math.exp(z))
+    return input
+def activationTanH(input):
+    for a in range(0,len(input)):
+        z=input[a]
+        input[a]=math.tanh(z)
+    return input
+def activationReLU(input):
+    for a in range(0,len(input)):
+        z=input[a]
+        if(z<0):
+            input[a]=0
+    return input
+def derivReLU( input):
+    for a in range(0,len(input)):
+        z=input[a]
+        if(z<0):
+            input[a]=0
+        else:
             input[a]=1
-        return input
+    return input
+def activationLeakyReLU( input):
+    b=0.01
+    for a in range(0,len(input)):
+        z=input[a]
+        if(z<0):
+            input[a]=z*b
+    return input
+def derivLeakyReLU( input):
+    for a in range(0, len(input)):
+        z=input[a]
+        if(z<0):
+            input[a]=0.01
+        else:
+            input[a]=1
+    return input
+def activationRandomReLU( input,b):
+    for a in range(0,len(input)):
+        z=input[a]
+        if(z<0):
+            input[a]=z*b
+    return input
+def derivRandomReLU( input, b):
+    for a in range(0,len(input)):
+        z=input[a]
+        if(z<0):
+            input[a]=b
+        else:
+            input[a]=1
+    return input
+def activationStep( input):
+    for a in range(0,len(input)):
+        z=input[a]
+        if(z<0):
+            input[a]=0
+        else:
+            input[a]=1
+    return input
+def activationArcTan( input):
+    for a in range(0,len(input)):
+        input[a]=numpy.arctan(input[a])
+    return input
+def activationSoftPlus( input):
+    for a in range(0,len(input)):
+        z=input[a]
+        b=math.log(1+math.exp(z))
+        input[a]=b
+    return input
+def activationELU( input, b):
+    for a in range(0,len(input)):
+        z=input[a]
+        if(z<0):
+            q=b*(math.exp(z)-1)
+            input[a]=q
+    return input
+def activationIdentity( input):
+    return input
+def derivIdentity( input):
+    for a in range(0,len(input)):
+        input[a]=1
+    return input
